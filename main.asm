bf2f935db8e59ae3ffc3fca60867cb48ba13ff3f
Victor <albu.victor.m@gmail.com>
2026-03-18 09:28:51 +0200

    added the basics for vulkan

--- main.asm
+++ main.asm
@@ -3,16 +3,39 @@ default rel
 extern GetModuleHandleA
 extern ExitProcess
 extern MessageBoxA
+extern vkCreateInstance
+extern vkDestroyInstance
 
 section .data
-    title db "Hello", 0
-    text  db "Hello from x64 Assembly!", 0
+    title: db "App", 0
+    text:  db "ok", 0
+
+    instance: dq 0
+    align 8
+    vk_struct_type_instance_info: 
+        dd 1
+        align 8
+        dq 0
+        dd 0
+        dq 0 
+        dd 0
+        align 8
+        dq 0
+        dd 0
+        align 8
+        dq 0
 
 section .text
 global main
 
 main:
-    sub rsp, 40             
+    sub rsp, 40 
+
+    lea rcx, [vk_struct_type_instance_info]
+    xor rdx, rdx
+    lea r8, [instance] 
+
+    call vkCreateInstance           
 
     xor rcx, rcx          
     lea rdx, [text]         
@@ -20,6 +43,11 @@ main:
     mov r9,  0              
 
     call MessageBoxA
+
+    lea rcx, [instance]
+    xor rdx, rdx
+
+    call vkDestroyInstance
 
     xor rcx, rcx
     call ExitProcess
--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 nasm -f win64 main.asm -o main.o
 gcc -m64 main.o -o main.exe -lkernel32 -luser32
+gcc main.o -o app.exe -L"C:\VulkanSDK\1.4.341.1\Lib" -lvulkan-1 -luser32 -lkernel32
