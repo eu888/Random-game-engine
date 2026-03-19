e4127d89a0ab9d247b46f4682d8553847c0140c8
Victor <albu.victor.m@gmail.com>
2026-03-19 14:52:56 +0200

    added more

--- main.asm
+++ main.asm
@@ -38,6 +38,168 @@ section .data
         
         dq 0 ;ppEnabledExtensionNames
 
+    DeviceProperties:
+        dd 0 ;apiVersion
+        dd 0 ;padding
+
+        dd 0 ;driverVersion
+        dd 0 ;padding
+
+        dd 0 ;vendorID
+        dd 0 ;padding
+
+        dd 0 ;deviceID
+        dd 0 ;padding
+
+        dd 0 ;deviceType
+        dd 0 ;padding
+
+        times 256 db 0 ;deviceName
+        
+        times 16 db 0 ;pipelineCacheUUID
+
+        times 226 dd 0 ;props
+        ;sparseProperties
+
+    props:
+        ; --- VkPhysicalDeviceProperties basic fields ---
+        dd 0      ; apiVersion
+        dd 0 ;padding
+        dd 0      ; driverVersion
+        dd 0 ;padding
+        dd 0      ; vendorID
+        dd 0 ;padding
+        dd 0      ; deviceID
+        dd 0 ;padding
+        dd 0      ; deviceType
+        dd 0 ;padding
+        times 256 db 0 ; deviceName[256]
+        times 16 db 0  ; pipelineCacheUUID[16]
+
+        ; --- VkPhysicalDeviceLimits ---
+        dd 0      ; maxImageDimension1D
+        dd 0 ;padding
+        dd 0      ; maxImageDimension2D
+        dd 0 ;padding
+        dd 0      ; maxImageDimension3D
+        dd 0 ;padding
+        dd 0      ; maxImageDimensionCube
+        dd 0 ;padding
+        dd 0      ; maxImageArrayLayers
+        dd 0 ;padding
+        dd 0      ; maxTexelBufferElements
+        dd 0 ;padding
+        dd 0      ; maxUniformBufferRange
+        dd 0 ;padding
+        dd 0      ; maxStorageBufferRange
+        dd 0 ;padding
+        dd 0      ; maxPushConstantsSize
+        dd 0 ;padding
+        dd 0      ; maxMemoryAllocationCount
+        dd 0 ;padding
+        dd 0      ; maxSamplerAllocationCount
+        dd 0 ;padding
+        dq 0      ; bufferImageGranularity (VkDeviceSize)
+        dq 0      ; sparseAddressSpaceSize (VkDeviceSize)
+        dd 0      ; maxBoundDescriptorSets
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorSamplers
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorUniformBuffers
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorStorageBuffers
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorSampledImages
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorStorageImages
+        dd 0 ;padding
+        dd 0      ; maxPerStageDescriptorInputAttachments
+        dd 0 ;padding
+        dd 0      ; maxPerStageResources
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetSamplers
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetUniformBuffers
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetUniformBuffersDynamic
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetStorageBuffers
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetStorageBuffersDynamic
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetSampledImages
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetStorageImages
+        dd 0 ;padding
+        dd 0      ; maxDescriptorSetInputAttachments
+        dd 0 ;padding
+        dd 0      ; maxVertexInputAttributes
+        dd 0 ;padding
+        dd 0      ; maxVertexInputBindings
+        dd 0 ;padding
+        dd 0      ; maxVertexInputAttributeOffset
+        dd 0 ;padding
+        dd 0      ; maxVertexInputBindingStride
+        dd 0 ;padding
+        dd 0      ; maxVertexOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationGenerationLevel
+        dd 0 ;padding
+        dd 0      ; maxTessellationPatchSize
+        dd 0 ;padding
+        dd 0      ; maxTessellationControlPerVertexInputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationControlPerVertexOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationControlPerPatchOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationControlTotalOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationEvaluationInputComponents
+        dd 0 ;padding
+        dd 0      ; maxTessellationEvaluationOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxGeometryShaderInvocations
+        dd 0 ;padding
+        dd 0      ; maxGeometryInputComponents
+        dd 0 ;padding
+        dd 0      ; maxGeometryOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxGeometryOutputVertices
+        dd 0 ;padding
+        dd 0      ; maxGeometryTotalOutputComponents
+        dd 0 ;padding
+        dd 0      ; maxFragmentInputComponents
+        dd 0 ;padding
+        dd 0      ; maxFragmentOutputAttachments
+        dd 0 ;padding
+        dd 0      ; maxFragmentDualSrcAttachments
+        dd 0 ;padding
+        dd 0      ; maxFragmentCombinedOutputResources
+        dd 0 ;padding
+        dd 0      ; maxComputeSharedMemorySize
+        dd 0 ;padding
+        times 3 dd 0 ; maxComputeWorkGroupCount[3]
+        dd 0 ;padding
+        dd 0      ; maxComputeWorkGroupInvocations
+        dd 0 ;padding
+        times 3 dd 0 ; maxComputeWorkGroupSize[3]
+        dd 0 ;padding
+        dd 0      ; subPixelPrecisionBits
+        dd 0 ;padding
+        dd 0      ; subTexelPrecisionBits
+        dd 0 ;padding
+        dd 0      ; mipmapPrecisionBits
+        dd 0 ;padding
+        dd 0      ; maxDrawIndexedIndexValue
+        dd 0 ;padding
+        dd 0      ; maxDrawIndirectCount
+        dd 0 ;padding
+        dd 0      ; padding if needed for alignment
+        dd 0 ;padding
+        dd 0      ; continue for floats and remaining fields...
+        dd 0 ;padding
+
 section .bss
     gpu_handles: resq 4
 
@@ -51,6 +213,7 @@ section .text
     init_vulkan:
         call createInstance
         call pickPhysicalDevice
+        call connectPhysicalDevice
         call no_error
         ret
 
@@ -76,7 +239,7 @@ section .text
         test rax, rax
         jnz error
 
-        mov rdx, [instance]
+        mov rcx, [instance]
         lea rdx, [gpu_count]
         lea r8, [gpu_handles]
         call vkEnumeratePhysicalDevices
